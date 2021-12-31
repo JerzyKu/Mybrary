b59a422d8cc666552efe7befcb491fdf4d3cfea2
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2021-12-31 10:40:33 +0100

    end of: Books Index/Create/New Routes - Node.js/Express/MongoDB Course #3

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req, res) => {
-    res.render("index")
+
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = await Book.find({}).sort({ "createdAt": 'desc' }).limit(3).exec() // all
+
+    } catch (e){
+        books = []
+    }
+    res.render("index", {books: books })
+
 })
 
 module.exports = router
